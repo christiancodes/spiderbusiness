89331ebeb4df18b9bf7cb91be7702046c875f05b
Christian DeWolf <christian@wolfbutler.com>
2016-01-31 20:16:09 -0400

    Add a simple clock

--- business.js
+++ business.js
@@ -21,6 +21,42 @@ Game.draw = function() {
 
 };
 
+// Pizza/clock mechanics
+
+function initializeClock(id){
+  var endtime = new Date();
+  endtime = addMinutes(endtime, 60);
+  var clock = document.getElementById(id);
+  var timeinterval = setInterval(function(){
+    var t = getTimeRemaining(endtime);
+    clock.innerHTML = ('0' + t.minutes).slice(-2) + ':' + ('0' + t.seconds).slice(-2);
+    if(t.total<=0){
+      clearInterval(timeinterval);
+    }
+  },1000);
+}
+
+function addMinutes(date, minutes) {
+    return new Date(date.getTime() + minutes*60000);
+}
+
+function getTimeRemaining(endtime){
+  var t = Date.parse(endtime) - Date.parse(new Date());
+  var seconds = Math.floor( (t/1000) % 60 );
+  var minutes = Math.floor( (t/1000/60) % 60 );
+  var hours = Math.floor( (t/(1000*60*60)) % 24 );
+  var days = Math.floor( t/(1000*60*60*24) );
+  return {
+    'total': t,
+    'days': days,
+    'hours': hours,
+    'minutes': minutes,
+    'seconds': seconds
+  };
+}
+
+//////////////////
+
 (function() {
   var onEachFrame;
   if (window.webkitRequestAnimationFrame) {
